297f47cc284c14f58bc883603931b06701f3b7e3
Juan Mora <jmora@serv.externos.prismamp.com>
2023-08-01 12:20:32 -0400

    feat: show heroes with name

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -1,5 +1,66 @@
+import { useLocation, useNavigate } from "react-router-dom";
+import { useForm } from "../../hooks/useform";
+import { HeroCard } from "../components/HeroCard";
+import queryString from "query-string";
+import { getHeroesByName } from "../helpers/getHeroesByName";
+
 export const SearchPage = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const { q = "" } = queryString.parse(location.search);
+
+  const heroes = getHeroesByName(q);
+
+  const { searchText, onInputChange } = useForm({ searchText: q });
+
+  const onSearchSubmit = (event) => {
+    event.preventDefault();
+    navigate(`?q=${searchText}`);
+  };
+
   return (
-    <h1>Search</h1>
-  )
-}
+    <>
+      <h1>Search</h1>
+      <hr />
+      <div className="row">
+        <div className="col-5">
+          <h4>Searching</h4>
+          <hr />
+          <form onSubmit={onSearchSubmit}>
+            <input
+              type="text"
+              placeholder="Search a hero"
+              className="form-control"
+              name="searchText"
+              autoComplete="off"
+              value={searchText}
+              onChange={onInputChange}
+            />
+            <button className="btn btn-outline-primary" type="submit">
+              Search
+            </button>
+          </form>
+        </div>
+        <div className="col-7">
+          <h4>Results</h4>
+          <hr />
+
+          {q === "" ? (
+            <div className="alert alert-primary">Search a hero:</div>
+          ) : (
+            heroes.length === 0 && (
+              <div className="alert alert-danger">
+                No hero with <b>{q}</b>
+              </div>
+            )
+          )}
+
+          {heroes.map((hero) => {
+            return <HeroCard key={hero.id} {...hero} />;
+          })}
+        </div>
+      </div>
+    </>
+  );
+};
